a793679f6483a7a4fd475bd6940b653b3933857b
Pranja1045 <pranjalsharma456778@gmail.com>
2023-12-15 11:05:22 +0530

    Create ques1.py

---
+++ ques1.py
@@ -0,0 +1,20 @@
+#1768. Merge Strings Alternately
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        temp=''
+        
+        if len(word1)>=len(word2):
+            for i in range(len(word1)):
+                temp+=word1[i]
+                if i<len(word2):
+                    temp+=word2[i]
+                else:
+                    temp+=word2[i:]
+        else:
+            for j in range(len(word2)):
+                if j<len(word1):
+                    temp+=word1[j]
+                else:
+                    temp+=word1[j:]
+                temp+=word2[j]
+        return temp
